67ffe5a6b4189d9b55bc6596cbb7e711fc32da46
Askild Olsen <askild@xangeli.com>
2018-05-29 12:40:43 +0200

    Stor bokstav i Digitalisert

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace digitalisert
+namespace Digitalisert
 {
     public class Startup
     {
