5918cb333488da9cd8a174a5838e948308660244
Askild Olsen <askild@xangeli.com>
2019-05-20 23:03:39 +0200

    Bruk Leaflet og Wicket for å vise WKT

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿// Write your JavaScript code.
+﻿$(".map").each(function() {
+    var wkt = new Wkt.Wkt();
+    var polygon = wkt.read($(this).text()).toObject();
+    $(this).empty();
+    var map = L.map($(this).get(0));
+    L.tileLayer('https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo4&zoom={z}&x={x}&y={y}', {
+        attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
+    }).addTo(map);
+    polygon.addTo(map);
+    map.fitBounds(polygon.getBounds());
+});
