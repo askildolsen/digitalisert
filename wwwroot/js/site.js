12b416d1416d46aef08429b867bd7d009b0fa7f0
Askild Olsen <askild@xangeli.com>
2019-06-29 20:37:58 +0200

    Bruk circle for point

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,11 +1,14 @@
 ﻿$(".map").each(function() {
-    var wkt = new Wkt.Wkt();
-    var polygon = wkt.read($(this).text()).toObject();
+    var wkt = new Wkt.Wkt().read($(this).text());
     $(this).empty();
-    var map = L.map($(this).get(0));
+
+    var polygon = (wkt.type == "point") ? L.circle([wkt.components[0].y, wkt.components[0].x], { radius: 3 }) : wkt.toObject();
+    
+    var map = L.map($(this).get(0)).setView([60.14, 10.25], 11);
+    polygon.addTo(map);
+    map.fitBounds(polygon.getBounds());
+
     L.tileLayer('https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo4&zoom={z}&x={x}&y={y}', {
         attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
     }).addTo(map);
-    polygon.addTo(map);
-    map.fitBounds(polygon.getBounds());
 });
