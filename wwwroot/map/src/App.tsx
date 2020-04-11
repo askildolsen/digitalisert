cb67bf47d9babc211c2150c5254448b1c5c65053
Askild Olsen <askild@xangeli.com>
2020-04-11 17:58:21 +0200

    Unngå nøstet retur-array med flatMap

--- wwwroot/map/src/App.tsx
+++ wwwroot/map/src/App.tsx
@@ -20,36 +20,36 @@ function App({url} : any) {
   const [markers, setMarkers] = useState();
 
   useEffect(() => {
-    const fetchData = async (url: string) => {
+    const fetchResourceData = async (url: string) => {
       const resources = await (await fetch(url)).json();
       const responsemarkers =
-        resources.map((resource: any, rindex: Number) => {
-          return resource.properties.filter((p: any) => p.tags.includes("@wkt")).map((property: any, pindex: Number) => {
-            return property.value.map((value: string, vindex: Number) => {
-              var wkt = new Wkt.Wkt().read(value);
+        resources.flatMap((resource: any, rindex: Number) => {
+          return resource.properties.filter((p: any) => p.tags.includes("@wkt")).flatMap((property: any, pindex: Number) => {
+            return property.value.flatMap((value: string, vindex: Number) => {
+              const wkt = new Wkt.Wkt().read(value);
               if (wkt.type === "point") {
-                return (
+                return [
                   <Marker key={rindex + "-" + pindex + "-" + vindex} position={[wkt.components[0].y, wkt.components[0].x]} icon={L.divIcon()}>
                     <Popup key={"pop-" + rindex + "-" + pindex + "-" + vindex}>
                       <ResourceCard resource={resource}/>
                     </Popup>
                   </Marker>
-                )
+                ]
               } else {
                 const polygonpositions = (wkt.components.length > 1)
                   ? wkt.components.map((co: any) => { return co.map((c: any) => { return [c.y, c.x] } ) })
                   : wkt.components[0].map((c: any) => { return [c.y, c.x] } );
-                return (
+                return [
                   <Polygon key={rindex + "-" + pindex + "-" + vindex} positions={ polygonpositions }>
                     <Popup key={"pop-" + rindex + "-" + pindex + "-" + vindex}>
                       <ResourceCard resource={resource}/>
                     </Popup>
                   </Polygon>
-                )
+                ]
               }
             });
           });
-        }).reduce((x: any,y: any) => x.concat(y), []).reduce((x: any, y: any) => x.concat(y), []);
+        });
 
         const positions = [].concat(...responsemarkers.map((m: any) => (m.props.position) ? [m.props.position] : m.props.positions));
         if (positions.length > 1 && positions.some((p1: any) => positions.some((p2: any) => p1.join('|') !== p2.join('|')))) {
@@ -62,7 +62,7 @@ function App({url} : any) {
         setMarkers(responsemarkers);
     }
 
-    fetchData(url);
+    fetchResourceData(url);
   }, [url]);
 
   const styles : CSSProperties = { position: 'absolute', top: 0, bottom:'0', width: '100%' };
